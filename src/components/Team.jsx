46149e4d5e24c6ebbc05eba3daa016f201b79fd3
Ali Hamza Sultan <122203689+alihamzasultan@users.noreply.github.com>
2025-03-15 09:03:49 +0500

    Add files via upload

---
+++ src/components/Team.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect } from "react";
+import team1 from "../img/1.jpg";
+import team2 from "../img/2.jpg";
+import team3 from "../img/3.jpg";
+import team4 from "../img/4.jpg";
+import team5 from "../img/5.jpg";
+import team6 from "../img/6.jpg";
+
+// AOS
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// custom styles
+import "./Team.css";
+import TeamImage from "./TeamImage";
+
+const Team = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1200,
+    });
+  }, []);
+
+  return (
+    <div className="text-[#7A6960] py-20">
+      {/* Section Heading */}
+      <h1 data-aos="fade-right" className="text-3xl font-semibold text-center">
+        Team
+      </h1>
+      <div className="flex justify-center items-center mt-2">
+        <div className="bg-[#2A9D8F] h-1 w-16 rounded"></div>
+      </div>
+      <p className="text-gray-700 text-center mt-7 mb-10 text-lg">
+        Our Mission is clear. We are building to train over 10 Crores Career
+        Aspirants
+      </p>
+
+      {/* Team Members Grid */}
+      <div className="flex flex-col xs:flex-row gap-6 p-5 lg:p-0 items-center">
+        {/* Roy Anderson - Marketing Director */}
+        <TeamImage
+          image={team1}
+          name={"Roy Anderson"}
+          role={"Marketing Director"}
+          dataAos="fade-up"
+        />
+        {/* Bri Fox - Sr Graphic Designer */}
+        <TeamImage
+          image={team2}
+          name={"Bri Fox"}
+          role={"Sr Graphic Designer"}
+          dataAos="fade-up"
+          dataAosDelay="100"
+        />
+        {/* Cathey Watkins - Customer Success Manager */}
+        <TeamImage
+          image={team3}
+          name={"Cathey Watkins"}
+          role={"Customer Success Manager"}
+          dataAos="fade-up"
+          dataAosDelay="200"
+        />
+        {/* Dionte Johnson - Sr Web Developer */}
+        <TeamImage
+          image={team4}
+          name={"Dionte Johnson"}
+          role={"Sr Web Developer"}
+          dataAos="fade-up"
+          dataAosDelay="300"
+        />
+        {/* Kyle Evans - SEO Specialist */}
+        <TeamImage
+          image={team5}
+          name={"Kyle Evans"}
+          role={"SEO Specialist"}
+          dataAos="fade-up"
+          dataAosDelay="400"
+        />
+        {/* Alicia Silvers - Sr Account Manager */}
+        <TeamImage
+          image={team6}
+          name={"Alicia Silvers"}
+          role={"Sr Account Manager"}
+          dataAos="fade-up"
+          dataAosDelay="500"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Team;
