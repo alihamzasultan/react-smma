42e8ff3923f251d715623de196bc21256c4c4017
Ali Hamza Sultan <122203689+alihamzasultan@users.noreply.github.com>
2025-03-20 15:22:28 +0500

    Update Routings.jsx

--- utils/Routings.jsx
+++ utils/Routings.jsx
@@ -2,10 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 // import { Redirect } from "react-router-dom";
 import Home from "../src/components/Home";
-import AppDevelopment from "../src/services/AppDevelopment";
-import WebDevelopment from "../src/services/WebDevelopment";
-import Seo from "../src/services/Seo";
-import Campaign from "../src/services/Campaign";
+
 
 
 
@@ -16,11 +13,6 @@ const Routings = () => {
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/app-development" element={<AppDevelopment />} />
-        <Route path="/web-development" element={<WebDevelopment />} />
-        <Route path="/seo-optimization" element={<Seo />} />
-        <Route path="/campagin-creation" element={<Campaign />} />
-        <Route path="/hireus" element={<Hireus />} />
 
       </Routes>
     </div>
