943859960ea3d2cb40bb767a5e801a0354adaca3
Ali Hamza Sultan <122203689+alihamzasultan@users.noreply.github.com>
2025-03-22 12:44:16 +0500

    Update Routings.jsx

--- utils/Routings.jsx
+++ utils/Routings.jsx
@@ -4,14 +4,18 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../src/components/Home";
 
 
-
-
+import Blog from "../src/components/BlogSection";
+import BlogDetails from "../src/components/BlogDetails";
 
 const Routings = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/" element={<Blog />} />
+        <Route path="/blog/:id" element={<BlogDetails />} />
+
+
 
       </Routes>
     </div>
